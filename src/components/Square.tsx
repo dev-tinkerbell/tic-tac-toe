9ec084c36acb857d5f696ad69608835d255063de
dev-tinkerbell <dev-tinkerbell@gmail.com>
2023-06-26 01:22:26 +0900

    tic-tac-toe typescript

---
+++ src/components/Square.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {
+  value: string | null;
+  onSquareClick: () => void;
+};
+
+export default function Square({ value, onSquareClick }: Props) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
