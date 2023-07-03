f9b0371855a49b2c11f9e426864346646682f4fe
Claire.kim <claire.kim@cheilpengtai.com>
2023-07-03 13:39:45 +0900

    코드 일부 수정

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
 type Props = {
-  value: string | null;
-  onSquareClick: () => void;
+    value: string | null;
+    onSquareClick: () => void;
 };
 
 export default function Square({ value, onSquareClick }: Props) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
+    return (
+        <button className='square' onClick={onSquareClick}>
+            {value}
+        </button>
+    );
 }
